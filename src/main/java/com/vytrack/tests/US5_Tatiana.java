166a0eafd121b744eb03b5d2e82862b477dec547
tvasylyniuk <tatianavasylyniuk@gmail.com>
2022-03-13 15:41:45 -0400

    US5 passed

--- src/main/java/com/vytrack/tests/US5_Tatiana.java
+++ src/main/java/com/vytrack/tests/US5_Tatiana.java
@@ -4,12 +4,19 @@ import com.vytrack.tests.base.TestBase;
 import com.vytrack.utils.BrowserUtils;
 import com.vytrack.utils.Driver;
 import com.vytrack.utils.VytrackUtils;
+import com.vytrack.utils.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class US5_Tatiana extends TestBase {
+
     @Test
     public void verify_columns_as_sales_manager(){
         //login as sales manager
@@ -28,11 +35,25 @@ public class US5_Tatiana extends TestBase {
         //Locate vehicles model
         WebElement vehiclesModelBtn = Driver.getDriver().findElement
                 (By.xpath("//span[normalize-space()='Vehicles Model' and contains(@class, 'title title-level-2')]"));
+
+        //we are using for waiting until loader mask disappears
+        VytrackUtils.waitTillLoaderMaskDisappear();
         vehiclesModelBtn.click();
+        VytrackUtils.waitTillLoaderMaskDisappear();
 
-
-
-
-
+        List<String> actualHeaderFromVehiclesModelTable = WebTableUtils.getHeadersFromVehicleModelTable();
+        List<String> expectedHeaderFromVehiclesModelTable = new ArrayList<>(Arrays.asList(
+                "MODEL NAME",
+                "MAKE",
+                "CAN BE REQUESTED",
+                "CVVI",
+                "CO2 FEE (/MONTH)",
+                "COST (DEPRECIATED)",
+                "TOTAL COST (DEPRECIATED)",
+                "CO2 EMISSIONS",
+                "FUEL TYPE",
+                "VENDORS"
+        ));
+        Assert.assertEquals(actualHeaderFromVehiclesModelTable,expectedHeaderFromVehiclesModelTable);
     }
 }
